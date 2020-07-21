0a29b18026b7dbb455192b60653e980b1a3c18e8
Abigail <abigail@abigail.be>
2020-07-22 00:41:34 +0200

    Initial setup for second page.

---
+++ Trapped-Knights/Src/place-knight.js
@@ -0,0 +1,17 @@
+#% Template: board
+var spiral;
+
+function loaded () {
+    spiral = new Spiral ({});
+
+    spiral . draw ();
+
+    spiral . place_values ({from_value: 1,
+                           to_value: 225,});
+
+    nav . add_todo (function () {
+        $('#number-1') . css ('display', 'none');
+    })
+}
+
+window . onload = loaded;

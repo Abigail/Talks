71a46b415bbc087c44e15b06702f6cabc4dc4c86
Abigail <abigail@abigail.be>
2020-07-22 23:55:30 +0200

    First animation.
    Move the knight to the places it can move to, then quickly move
    it back to the center.

--- Trapped-Knights/JavaScript/knight.js
+++ Trapped-Knights/JavaScript/knight.js
@@ -4,4 +4,20 @@ function loaded () {
     piece = new Piece ({piece: 'knight'});
 
     piece . draw ();
+
+    var knight = SVG ('#chess-piece');
+
+    var offsets = [[-1, -2], [ 1, -2], [ 2, -1], [ 2,  1],
+                   [ 1,  2], [-1,  2], [-2,  1], [-2, -1]];
+
+    offsets . forEach (item => {
+        [x, y] = item;
+        knight . animate ({duration: 1000,
+                           delay:    1000,})
+               . dmove (  x * piece . rect_size,   y * piece . rect_size)
+
+        knight . animate ({duration:  100,
+                           delay:    1000,})
+               . dmove (- x * piece . rect_size, - y * piece . rect_size);
+    });
 }

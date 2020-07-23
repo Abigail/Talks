8e6758ef1cc519652c1d593fd61d0f2f501f13f9
Abigail <abigail@abigail.be>
2020-07-23 14:00:56 +0200

    Don't place more values than we have squares.

--- Trapped-Knights/JavaScript/empty-board.js
+++ Trapped-Knights/JavaScript/empty-board.js
@@ -5,21 +5,23 @@ function loaded () {
 
     board . draw ();
 
+    var max_value = board . max_value;
+
     nav . add_todo (function () {board . place_value (1)});
     nav . add_todo (function () {
-        board . place_values ({from_value:    2,
-                               to_value:      9,
-                               delay:       500})
+        board . place_values ({from_value:       2,
+                               to_value:         9,
+                               delay:          500})
     });
     nav . add_todo (function () {
-        board . place_values ({from_value:   10,
-                               to_value:     64,
-                               delay:       100})
+        board . place_values ({from_value:      10,
+                               to_value:        64,
+                               delay:          100})
     });
     nav . add_todo (function () {
-        board . place_values ({from_value:   65,
-                               to_value:    225,
-                               delay:        25})
+        board . place_values ({from_value:       65,
+                               to_value:  max_value,
+                               delay:            25})
     });
 
 }

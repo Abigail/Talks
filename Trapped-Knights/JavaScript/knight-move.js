6d2eb955748e0e7ef58e1f2632510f4e2996a5a9
Abigail <abigail@abigail.be>
2020-07-24 15:15:06 +0200

    knight-move.js: Move the knight till it falls of the board.

--- Trapped-Knights/JavaScript/knight-move.js
+++ Trapped-Knights/JavaScript/knight-move.js
@@ -7,15 +7,32 @@ function loaded () {
 
     spiral . place_values ({from_value: 2});
 
-    var knight = chess_pieces . knight;
+    let knight = chess_pieces . knight;
 
     spiral . place_piece (knight, {});
-    var moves = knight . run_list;
 
-    for (var i = 1; i <= 5; i ++) {
+    let moves = knight . run_list;
+
+    let manual_steps = 5;
+
+    for (var i = 1; i <=  manual_steps; i ++) {
         let move = moves [i];
         nav . add_todo (function () {
-            spiral . move_piece ({value: move})
+            spiral . move_piece ({move_list: [move]})
         });
     }
+
+    //
+    // We're using a trick here. We first delete the existing chess
+    // piece, then replace it with another. This way, the animation
+    // runs more smoothly; that is, it won't do the first steps all
+    // at once.
+    //
+    nav . add_todo (function () {
+        SVG ('#chess-piece') . css ({display: 'none'});
+        SVG ('#chess-piece') . id ('bogus');
+        spiral . place_piece (knight, {value: moves [manual_steps]});
+        spiral . move_piece ({move_list: moves,
+                              start:     manual_steps + 1,});
+    });
 }

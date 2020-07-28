6bb4a8a39c61fcefbd98515188dedbf360ac3371
Abigail <abigail@abigail.be>
2020-07-28 17:02:50 +0200

    Credit line for the knight.

--- Trapped-Knights/JavaScript/knight.js
+++ Trapped-Knights/JavaScript/knight.js
@@ -4,4 +4,15 @@ function loaded () {
     piece = new Piece ({piece: chess_pieces . knight});
 
     piece . draw ();
+
+    let credits = chess_pieces . knight . credits;
+
+    let line;
+
+    line = "<a href = '" + credits . link         + "'>"     +
+                           credits . name         + "</a>. " +
+           "<a href = '" + credits . licence_link + "'>"     +
+                           credits . licence      + "</a>";
+
+    $('.credits') . html ("Chess piece credits: " + line);
 }

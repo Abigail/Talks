acf4cbd65afa9dbb64c076ee58c59fb8846d2113
Abigail <abigail@abigail.be>
2020-07-28 23:24:34 +0200

    Merged knight-move.js into piece.js
    And made it generic -- it will do the right thing depending on
    the given parameter.

--- Trapped-Knights/JavaScript/piece.js
+++ Trapped-Knights/JavaScript/piece.js
@@ -16,3 +16,56 @@ function piece_movements (name) {
 
     make_credits_line (name);
 }
+
+
+//
+// move_piece:
+//    Create a board, center the piece in the middle. Move the piece
+//    one step for a few iteration, then animate the rest until it
+//    runs out of bounds.
+//
+function move_piece (name) {
+    let piece  = chess_pieces [name];
+    let spiral = new Spiral ({});
+
+    spiral . draw ({with_values: 1});
+
+    spiral . place_piece (piece, {});
+    spiral . hide_value (1);
+
+    let moves = piece . run_list;
+
+    let manual_steps = piece . manual_moves || 0;
+
+    for (let i = 1; i <= manual_steps; i ++) {
+        nav . add_todo (function () {
+            spiral . move_piece ({
+                move_list: moves,
+                start:     i,
+                end:       i,
+                callback:  function (args) {
+                    $('#move-number') . html (args . move);
+                }
+            });
+        });
+    }
+
+    //
+    // We're using a trick here. We first delete the existing chess
+    // piece, then replace it with another. This way, the animation
+    // runs more smoothly; that is, it won't do the first steps all
+    // at once.
+    //
+    nav . add_todo (function () {
+        SVG ('#chess-piece') . css ({display: 'none'});
+        SVG ('#chess-piece') . id ('bogus');
+        spiral . place_piece (piece, {value: moves [manual_steps]});
+        spiral . move_piece ({
+            move_list: moves,
+            start:     manual_steps + 1,
+            callback:  function (args) {
+                $('#move-number') . html (args . move);
+            }
+        });
+    });
+}

0e31027953170fa4af185717998d74772d394329
Abigail <abigail@abigail.be>
2020-07-28 22:21:38 +0200

    piece.js: Add some comments.

--- Trapped-Knights/JavaScript/piece.js
+++ Trapped-Knights/JavaScript/piece.js
@@ -1,3 +1,14 @@
+//
+// This contains the various functions which are called from the
+// loaded () method -- which is the method being called after
+// the page was loaded.
+//
+
+
+//
+// piece_movements:
+//   Method to set up the image which shows the movement of the piece.
+//
 function piece_movements (name) {
     piece = new Piece ({piece: chess_pieces [name]});
 
